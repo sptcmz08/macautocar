8fa85a4100262beeb5734e47fcc820bd035c2a02
Werachai Nachiangmai <ae1nt14@gmail.com>
2026-03-08 18:00:14 +0700

    แก้ปัญหาบันทึกไม่ได้: เอา nested form ออกจาก edit modal + เพิ่ม deleteRefurb JS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1477,10 +1477,10 @@
                 </table>
             </div>
 
-            </div>
+        </div>
 
         <script>
-        window.carPaginationRender = function() {};
+        w                indow .carP                aginationRender = function() {};
         </script>
         <div id="partSection" class="hidden">
             <div class="bg-white shadow rounded-lg overflow-x-auto table-responsive">
@@ -3671,7 +3671,7 @@
     <!-- Car Edit Modals (one for each car) -->
     @foreach($cars as $car)
         @php 
-                                                                                                                                    $totalCost = $car->total_cost;
+                                                                                                                                                    $totalCost = $car->total_cost;
             $expectedProfit = $car->selling_price ? ($car->selling_price - $totalCost) : 0;
         @endphp
         <div id="editCarModal{{ $car->id }}"
@@ -3791,15 +3791,12 @@
                                         <span class="text-sm">{{ $item->name }}</span>
                                         <div class="flex items-center gap-3">
                                             <span class="text-sm text-orange-600 font-medium">฿{{ number_format($item->amount, 0) }}</span>
-                                            <form action="{{ route('refurbishments.destroy', $item->id) }}" method="POST" onsubmit="return confirm('ลบรายการนี้?');">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
-                                                        <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clip-rule="evenodd" />
-                                                    </svg>
-                                                </button>
-                                            </form>
+                                            <button type="button" onclick="deleteRefurb({{ $item->id }})"
+                                                class="text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                                    <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clip-rule="evenodd" />
+                                                </svg>
+                                            </button>
                                         </div>
                                     </div>
                                 @endforeach
@@ -3873,6 +3870,12 @@
 
     <!-- Hidden form for image deletion -->
     <form id="imageDeleteForm" method="POST" class="hidden">
+        @csrf
+        @method('DELETE')
+    </form>
+
+    <!-- Hidden form for refurbishment deletion -->
+    <form id="refurbDeleteForm" method="POST" class="hidden">
         @csrf
         @method('DELETE')
     </form>
@@ -4315,6 +4318,14 @@
             if (confirm('ต้องการลบรูปภาพนี้หรือไม่?')) {
                 var form = document.getElementById('imageDeleteForm');
                 form.action = '/car-images/' + imageId;
+                form.submit();
+            }
+        }
+
+        function deleteRefurb(refurbId) {
+            if (confirm('ลบรายการปรับสภาพนี้?')) {
+                var form = document.getElementById('refurbDeleteForm');
+                form.action = '/refurbishments/' + refurbId;
                 form.submit();
             }
         }
